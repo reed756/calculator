88bfad1634ca20ba93c40f9097c213dd84b04e7c
reed756 <68922323+reed756@users.noreply.github.com>
2021-01-28 20:28:47 +0000

    Working on getting the display and buttons working correctly

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* Your calculator is going to contain functions for all of the basic math operators you 
+typically find on simple calculators, so start by creating functions for the following 
+items and testing them in your browser’s console.
+add
+subtract
+multiply
+divide
+*/
+
 function add (a, b) {
 	return a + b;
 }
@@ -14,6 +23,9 @@ function divide (a , b) {
     return a / b;
 }
 
+/* Create a new function operate that takes an operator and 2 numbers and then calls 
+one of the above functions on the numbers. */
+
 function operate (operator, a, b) {
     switch (operator) {
         case add:
@@ -26,17 +38,66 @@ function operate (operator, a, b) {
             return divide(a, b);
     }
 }
+
+/*Create the functions that populate the display when you click the number buttons… 
+you should be storing the ‘display value’ in a variable somewhere for use in the 
+next step.*/
+
 const display = document.querySelector("#display");
-const one = document.querySelector("#one");
-const two = document.querySelector("#two");
+const buttons = document.querySelectorAll("button");
+const clear = document.getElementsByClassName("clear");
+display.textContent = "0";
+let displayValue1 = 0;
+let displayValue2 = 0;
+let operator = "";
 
-one.addEventListener('click', One);
-two.addEventListener('click', Two);
+    const buttonPress = buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            if (display.textContent === "0" && button.className === "numbers" && operator !== "add") {
+                display.textContent = "";
+                display.textContent = `${display.textContent}${button.id}`;
+                displayValue1 = `${button.id}`;
+                console.log(operator);
+            } else if (display.textContent !== "0" && button.className === "numbers" && operator !== "add") {
+                display.textContent = `${display.textContent}${button.id}`;
+                displayValue1 += `${button.id}`;
+                console.log(operator);
+            } else if (button.className === "clear") {
+                display.textContent = "0";
+                displayValue1 = 0;
+                displayValue2 = 0;
+                operator = "";
+            } else if (button.className = "operator") {
+                operator = `${button.id}`;
+                display.textContent = "0";
+                console.log(operator);
+            }
+        })
+    });
+/*
+clear.addEventListener("click", function clearDisplay(event) {
+    event.display.textContent = "";
+});
 
-function One() {
-    display.textContent = `${display.textContent}1`;
-}
+if (button.id != "" && button.id != "clear" && button.id != "add") {
+            display.textContent = "";
+            display.textContent = `${display.textContent}${button.id}`;
+            } else if (button.id === "clear") {
+                display.textContent = "";
+            } else if (button.id === "add") {
+                display.textContent = "";
+            }
 
-function Two() {
-    display.textContent = `${display.textContent}2`;
-}
+else if (operator === "add" && button.className === "numbers" && displayValue2 === 0) {
+                display.textContent = `${display.textContent}${button.id}`;
+                displayValue2 = `${button.id}`;
+                console.log(displayValue2);
+                console.log(operator);
+            } else if (operator === "add" && button.className === "numbers" && displayValue2 !== 0) {
+                display.textContent = `${display.textContent}${button.id}`;
+                displayValue2 += `${button.id}`;
+                console.log(displayValue2);
+                console.log(operator);
+            }
+
+*/
